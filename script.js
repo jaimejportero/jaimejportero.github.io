20508b2be21edc3420adc5657becd7ef31acdfb5
Jaime <jaimejportero@gmail.com>
2024-02-27 10:27:10 +0100

    Cambios de todo

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-
-document.getElementById('generateBtn').addEventListener('click', function() {
+document.getElementById('generateBtn').addEventListener('click', function () {
     // Generar número aleatorio del 1 al 100
     const randomNumber = Math.floor(Math.random() * 100) + 1;
-  
-    // Actualizar la barra de progreso
-    const fill = document.getElementById('fill');
-    fill.style.width = randomNumber + '%';
-  
+
     // Actualizar el porcentaje
     const percentage = document.getElementById('percentage');
     percentage.textContent = randomNumber + '%';
-  });
-  
+
+    // Actualizar la barra de progreso
+    const fill = document.getElementById('fill');
+    fill.style.width = randomNumber + '%';
+
+    // Realizar el efecto de latido del corazón
+    const heart = document.getElementById('heart');
+    heart.style.transform = 'scale(1.1)'; // Hacer el corazón más grande
+    setTimeout(function () {
+        heart.style.transform = 'scale(1)'; // Volver al tamaño original
+    }, 200); // Esperar 200 milisegundos antes de volver al tamaño original
+});
