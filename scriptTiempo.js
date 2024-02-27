0bea9135a323b15d776e56f75c039ea87db400e7
Jaime <jaimejportero@gmail.com>
2024-02-27 13:26:17 +0100

    dsa

--- scriptTiempo.js
+++ scriptTiempo.js
@@ -1,23 +1,27 @@
-const fechaInicio = new Date('2023-11-05');
-// Fecha actual del sistema
-const fechaActual = new Date();
-console.log(fechaActual);
-// Calcula la diferencia en milisegundos
-const diferencia = fechaActual - fechaInicio;
-console.log(diferencia);
+function actualizarTiempo() {
+    const fechaInicio = new Date('2023-11-05');
+    // Fecha actual del sistema
+    const fechaActual = new Date();
 
-// Convierte la diferencia de milisegundos a años, meses, días, horas, minutos y segundos
-const segundosTotales = diferencia / 1000;
-const minutosTotales = segundosTotales / 60;
-const horasTotales = minutosTotales / 60;
-const diasTotales = horasTotales / 24;
-const mesesTotales = diasTotales / 30.4375; // Aproximadamente, asumiendo 365.25 días por año
-const añosTotales = diasTotales / 365.25; // Año bisiesto cada 4 años
+    // Calcula la diferencia en milisegundos
+    const diferencia = fechaActual - fechaInicio;
 
-// Muestra el tiempo transcurrido en la página HTML
-document.getElementById('años').textContent = Math.floor(añosTotales);
-document.getElementById('meses').textContent = Math.floor(mesesTotales);
-document.getElementById('días').textContent = Math.floor(diasTotales);
-document.getElementById('horas').textContent = Math.floor(horasTotales);
-document.getElementById('minutos').textContent = Math.floor(minutosTotales);
-document.getElementById('segundos').textContent = Math.floor(segundosTotales);
+    // Convierte la diferencia de milisegundos a años, meses, días, horas, minutos y segundos
+    const segundosTotales = diferencia / 1000;
+    const minutosTotales = segundosTotales / 60;
+    const horasTotales = minutosTotales / 60;
+    const diasTotales = horasTotales / 24;
+    const mesesTotales = diasTotales / 30.4375; // Aproximadamente, asumiendo 365.25 días por año
+    const añosTotales = diasTotales / 365.25; // Año bisiesto cada 4 años
+
+    // Muestra el tiempo transcurrido en la página HTML
+    document.getElementById('años').textContent = Math.floor(añosTotales);
+    document.getElementById('meses').textContent = Math.floor(mesesTotales);
+    document.getElementById('días').textContent = Math.floor(diasTotales);
+    document.getElementById('horas').textContent = Math.floor(horasTotales);
+    document.getElementById('minutos').textContent = Math.floor(minutosTotales);
+    document.getElementById('segundos').textContent = Math.floor(segundosTotales);
+}
+
+// Actualiza el tiempo cada segundo
+setInterval(actualizarTiempo, 1000);
