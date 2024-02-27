87cee5f47ac36b11c7456d68060807c48bee3b7e
Jaime <jaimejportero@gmail.com>
2024-02-27 11:11:43 +0100

    Prueba final

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 document.getElementById('generateBtn').addEventListener('click', function () {
     // Generar número aleatorio del 1 al 100
     const randomNumber = Math.floor(Math.random() * 100) + 1;
-    const arrow = document.getElementById('arrow');
-    const rotationAngle = randomNumber * 1.8; // 180 grados divididos en 100 partes
-    arrow.style.transform = 'rotate(' + rotationAngle + 'deg)';
-    // const randomNumber = Math.floor(Math.random() * 100) + 1;
 
-    // // Actualizar el porcentaje gradualmente
-    // let currentPercentage = 0;
-    // const interval = setInterval(function () {
-    //     currentPercentage++;
-    //     const percentage = document.getElementById('percentage');
-    //     percentage.textContent = currentPercentage + '%';
-    //     const fill = document.getElementById('fill');
-    //     fill.style.width = currentPercentage + '%';
-    //     if (currentPercentage >= randomNumber) {
-    //         clearInterval(interval);
-    //     }
-    // }, 10);
+    // Actualizar el porcentaje gradualmente
+    let currentPercentage = 0;
+    const interval = setInterval(function () {
+        currentPercentage++;
+        const percentage = document.getElementById('percentage');
+        percentage.textContent = currentPercentage + '%';
+        const fill = document.getElementById('fill');
+        fill.style.width = currentPercentage + '%';
+        if (currentPercentage >= randomNumber) {
+            clearInterval(interval);
+        }
+    }, 10);
 
     // Realizar el efecto de latido del corazón
     const heart = document.getElementById('heart');
