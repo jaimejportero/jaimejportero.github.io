b0d09eb6179248fd795973f4c48408ad30154ebf
Jaime <jaimejportero@gmail.com>
2024-02-27 10:16:19 +0100

    CorazonPrueba1

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+document.getElementById('generateBtn').addEventListener('click', function() {
+    // Generar número aleatorio del 1 al 100
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+  
+    // Actualizar la barra de progreso
+    const fill = document.getElementById('fill');
+    fill.style.width = randomNumber + '%';
+  
+    // Actualizar el porcentaje
+    const percentage = document.getElementById('percentage');
+    percentage.textContent = randomNumber + '%';
+  });
+  
