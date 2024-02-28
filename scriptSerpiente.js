f61556509b991900a6d931793c4dc217fe8f2a8a
Jaime <jaimejportero@gmail.com>
2024-02-28 10:20:34 +0100

    dsa

---
+++ scriptSerpiente.js
@@ -0,0 +1,32 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const scale = 20;
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
+
+let snake;
+
+(function setup() {
+    snake = new Snake();
+    fruit = new Fruit();
+    fruit.pickLocation();
+
+    window.setInterval(() => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        fruit.draw();
+        snake.update();
+        snake.draw();
+
+        if (snake.eat(fruit)) {
+            fruit.pickLocation();
+        }
+
+        snake.checkCollision();
+        document.querySelector('.score').innerText = snake.total;
+    }, 250);
+}());
+
+window.addEventListener('keydown', e => {
+    const direction = e.key.replace('Arrow', '');
+    snake.changeDirection(direction);
+});
