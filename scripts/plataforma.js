186b74b62119909c77a48456add1b5d31636944e
Jaime <jaimejportero@gmail.com>
2024-03-01 12:11:01 +0100

    dsa

--- scripts/plataforma.js
+++ scripts/plataforma.js
@@ -1,6 +1,7 @@
 const holes = document.querySelectorAll('.hole');
 let interval = 1000; // Intervalo inicial de 1000ms
 let score = 0;
+
 function getRandomHole() {
   const index = Math.floor(Math.random() * holes.length);
   return holes[index];
@@ -12,7 +13,7 @@ function showMole() {
 
   setTimeout(() => {
     hole.classList.remove('mole');
-    interval *= 0.95; // Disminuir el intervalo en un 10% cada vez
+    interval *= 0.95; // Disminuir el intervalo en un 5% cada vez
     setTimeout(showMole, interval); // Cambiar el intervalo de aparición del topo
   }, interval); // Usar el intervalo actual
 }
@@ -21,15 +22,22 @@ holes.forEach(hole => {
   hole.addEventListener('click', () => {
     if (hole.classList.contains('mole')) {
       hole.classList.remove('mole');
-      score+=50; // Incrementa la puntuación
+      score += 50; // Incrementa la puntuación
       updateScore(); // Actualiza la puntuación en el HTML
-      // Incrementar puntaje u otras acciones
     }
   });
 });
+
 function updateScore() {
   const scoreElement = document.getElementById('score');
-  scoreElement.textContent = `Puntuación: ${score}`;
+  if (scoreElement) { // Verificar si el elemento existe antes de actualizar la puntuación
+    scoreElement.textContent = `Puntuación: ${score}`;
+  } else {
+    console.error('No se encontró el elemento con ID "score"');
+  }
 }
+
+// Comprobamos si el marcador de puntuación se actualiza correctamente al inicio
+updateScore();
 
 showMole(); // Comenzar a mostrar topos
