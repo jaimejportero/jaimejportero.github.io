e66f16401c70ae595c693e4b64e25461eb3b2a7a
Jaime <jaimejportero@gmail.com>
2024-03-05 10:07:29 +0100

    as

---
+++ paginas/index.js
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+// [START maps_map_simple]
+let map;
+
+async function initMap() {
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();
+// [END maps_map_simple]
